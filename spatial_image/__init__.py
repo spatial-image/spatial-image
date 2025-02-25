7ff44198ad2b9f460785911fd04460ef70c8afa6
Matt McCormick <matt@mmmccormick.com>
2025-02-24 20:22:12 -0500

    ENH: Bump version to 1.2.1

--- spatial_image/__init__.py
+++ spatial_image/__init__.py
@@ -2,7 +2,7 @@
 
 A multi-dimensional spatial image data structure for Python."""
 
-__version__ = "1.2.0"
+__version__ = "1.2.1"
 
 import logging
 from typing import Union, Optional, Sequence, Hashable, Tuple, Mapping, Any, Literal
